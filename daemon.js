fe4ecb03e8cba9ad02fe79b4013ab3f9ebaf27c8
Craig Taub <craigtaub@gmail.com>
2021-08-20 22:53:34 +0100

    Add run env. process from

--- daemon.js
+++ daemon.js
@@ -27,43 +27,55 @@ app.get("/:command/:args", async (req, res) => {
       const fullConfig = await import("./images/middle-layer/config.json");
       const config = fullConfig.default[0].Config;
       const splitCommand = config.Cmd;
+      // env is key:value pairs
+      const environment = config.Env.reduce((acc, curr) => {
+        const [key, value] = curr.split("=");
+        acc[key] = value;
+        return acc;
+      }, {});
       const workingDir = config.WorkingDir;
 
       // set working directory
-      console.log("workingDir", workingDir);
-      process.chdir(`tmp/${workingDir}`);
+      // process.chdir(`tmp/${workingDir}`);
 
       // run command in child
       const startCmd = splitCommand[0];
       splitCommand.shift();
-      const npm = spawn(startCmd, splitCommand);
-      npm.stdout.on("data", (data) => {
+      const childProcess = spawn(startCmd, splitCommand, {
+        cwd: `tmp/${workingDir}`,
+        env: environment,
+      });
+      childProcess.stdout.on("data", (data) => {
         console.log(`stdout: ${data}`);
       });
-      npm.stderr.on("data", (data) => {
+      childProcess.stderr.on("data", (data) => {
         console.error(`stderr: ${data}`);
       });
-      npm.on("close", (code) => {
+      childProcess.on("error", (error) => {
+        console.log(`child process error ${error}`);
+      });
+      childProcess.on("close", (code) => {
         console.log(`child process exited with code ${code}`);
       });
+      childProcess.unref();
       break;
     case "build":
       const buildImage = req.params.args;
       if (buildImage === ".") {
         // default local image
         const fullPath = path.resolve(path.dirname(""), "./OurDockerfile");
-        console.log("fullPath", fullPath);
         const file = await asyncReadFile(fullPath, {
           encoding: "utf-8",
         });
         // good for small files, NOT big ones
         const linesArray = file.split(/\r?\n/);
-        linesArray.map((line) => {
-          commitLine(line);
+        linesArray.map(async (line) => {
+          await commitLine(line);
         });
       }
       break;
   }
+
   res.send("success");
 });
 
@@ -71,6 +83,22 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-function commitLine(line) {
-  //
+async function commitLine(line) {
+  const args = line.split(" ");
+  const command = args[0];
+  args.shift();
+  // call command function
+  // await commitMap[command.toLowerCase()](args);
 }
+
+const commitMap = {
+  from: async (layer) => {
+    // move to tmp for processing
+    await asyncNcp(`images/${layer}`, `tmp`);
+    // change working dir
+    process.chdir(`tmp/${layer}`);
+  },
+  env: (values) => {
+    // push to Config.Env[]
+  },
+};
